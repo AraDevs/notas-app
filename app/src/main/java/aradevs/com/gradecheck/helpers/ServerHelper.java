d840944fd9138bb018f48357dabb79515c60b2d2
ar4 <ooskarargueta08@gmail.com>
2018-10-04 22:25:42 -0600

    Fix temporal url
    Es posible acceder al nuevo servlet de digitalocean

--- app/src/main/java/aradevs/com/gradecheck/helpers/ServerHelper.java
+++ app/src/main/java/aradevs/com/gradecheck/helpers/ServerHelper.java
@@ -5,7 +5,7 @@ package aradevs.com.gradecheck.helpers;
  */
 public class ServerHelper {
     //Declaring server endpoints
-    public static final String URL = "http://142.93.207.172:8080/POOSistema_NotasAPI-1.0-SNAPSHOT/";
+    public static final String URL = "http://104.248.67.79:8080/POOSistema_NotasAPI-1.0-SNAPSHOT/";
     public static final String COURSES = "RegisteredCourses/read/";
     public static final String USER = "Students/login";
 }
